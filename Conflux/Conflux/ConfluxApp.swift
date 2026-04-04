1958f9e4beaf5591119347e468cff4fd3f1b556c
Theerat Punyasit <vetozer6417@gmail.com>
2026-04-04 23:35:02 +0700

    Initialized project xcode

---
+++ Conflux/Conflux/ConfluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfluxApp.swift
+//  Conflux
+//
+//  Created by Theerat Punyasit on 4/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct ConfluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Conflux/Conflux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Conflux
+//
+//  Created by Theerat Punyasit on 4/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
