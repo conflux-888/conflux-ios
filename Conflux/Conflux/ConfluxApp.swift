f99040cf2881894f4336b4a90a9cba2587fbd124
Theerat Punyasit <vetozer6417@gmail.com>
2026-04-09 23:57:19 +0700

    firebase sdk

--- Conflux/Conflux/ConfluxApp.swift
+++ Conflux/Conflux/ConfluxApp.swift
@@ -1,8 +1,14 @@
 import SwiftUI
+import Firebase
+import FirebaseInAppMessaging
 
 @main
 struct ConfluxApp: App {
     @State private var authManager = AuthManager()
+
+    init() {
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
